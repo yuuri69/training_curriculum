9e48a493645946e67edf29f486fea157e06f6e81
yuuri69 <70687538+yuuri69@users.noreply.github.com>
2020-10-02 15:08:35 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,7 +2,7 @@ class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
   def index
-    getWeek
+    getweek
     @plan = Plan.new
   end
 
@@ -18,7 +18,7 @@ class CalendarsController < ApplicationController
     params.require(:plan).permit(:date, :plan)
   end
 
-  def getWeek
+  def getweek
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
@@ -27,16 +27,21 @@ class CalendarsController < ApplicationController
 
     @week_days = []
 
-    plans = Plan.where(date: @todays_date..@todays_date + 6)
+    @plans = Plan.where(date: @todays_date..@todays_date + 6)
 
     7.times do |x|
       today_plans = []
-      plan = plans.map do |plan|
+      plan = @plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
-      @week_days.push(days)
-    end
+      wday_num = Date.today.wday
 
+      if wday_num = x + 1
+        wday_num = wday_num - 7 
+       days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday:  wdays[wday_num] }
+        @week_days.push(days)
+      end
+    end
   end
 end
+
